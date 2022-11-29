8fd2ba3d6cc2512202cdec054f736259fe4b5eeb
adden <a.d.denusiv@mail.ru>
2022-11-29 17:27:49 +0300

    added users list loading and showing with recyclerView, added splash

---
+++ app/src/main/java/com/example/testlifehackstudio/presentation/splash/SplashFragment.kt
@@ -0,0 +1,19 @@
+package com.example.testlifehackstudio.presentation.splash
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.example.testlifehackstudio.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashFragment: Fragment(R.layout.fragment_splash) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        lifecycleScope.launch {
+            delay(2000)
+            findNavController().navigate(R.id.action_splashFragment_to_usersListFragment)
+        }
+    }
+}
